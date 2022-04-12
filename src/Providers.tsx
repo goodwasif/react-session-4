d7a279d37bb26b933f0322db2d46a97c908f6bd9
itxnaeem <naeem.akram@ideofuzion.com>
2022-04-12 21:51:48 +0500

    earily commit

---
+++ src/Providers.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { HelmetProvider } from 'react-helmet-async'
+import { Provider } from 'react-redux'
+import { ThemeContextProvider } from 'contexts/ThemeContext'
+import { RefreshContextProvider } from 'contexts/RefreshContext'
+import { ModalProvider } from 'widgets/Modal'
+
+const Providers: React.FC = ({ children }) => {
+  return (
+          <HelmetProvider>
+            <ThemeContextProvider>
+                <RefreshContextProvider>
+                  <ModalProvider>                    
+                      {children}
+                  </ModalProvider>
+                </RefreshContextProvider>
+            </ThemeContextProvider>
+          </HelmetProvider>
+  )
+}
+
+export default Providers
---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,16 @@
+import { Flex } from 'components/Box'
+import FlexLayout from 'components/layout/Flex'
+import React from 'react'
+
+const Home: React.FC = () => {
+  // const { currentBlock } = useBlock()
+  return (
+
+    <Flex flexDirection="column" mr={['8px', 0]}>
+
+      dd
+    </Flex>
+  )
+}
+
+export default Home
